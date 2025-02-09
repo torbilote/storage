575cb5b60f2d47fefcddb646ead6bebe4c248ba9
torbilote <norbert.lipinski96@gmail.com>
2025-02-09 15:11:30 +0100

    bash cheathsheet day 2

--- bash/cheatsheet.sh
+++ bash/cheatsheet.sh
@@ -21,3 +21,19 @@ Scripts are non-interactive.
 # To check if shell is interactive (and many other parameters) look if there is 'i' in the output.
 echo $-
 
+Bash supports 3 types of built-in commands:
+- sh built-in commands:
+    :, ., break, cd, continue, eval, exec, exit, export, getopts, hash, pwd, readonly, return, set, shift, test, [, times, trap, umask and unset.
+- bash built-in commands:
+    alias, bind, builtin, command, declare, echo, enable, help, let, local, logout, printf, read, shopt, type, typeset, ulimit and unalias.
+- special POSIX built-in commands:
+    ...
+
+When the program being executed is a shell script, bash will create a new bash process using a fork.
+This subshell reads the lines, interprets and executes as if they would come directly from the keyboard line by line.
+While subshell processes each line, the parent shell waits for its child to finish.
+When subshell terminates, the parent awakes and displays a new prompt.
+
+Shell script always starts with the same two characters "#!". After that, the shell that will execute the script commands is defined.
+Standard location of installed bash is /bin/bash.
+
